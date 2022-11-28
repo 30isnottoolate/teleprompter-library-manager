b2995eef235ad0c5671b6b4f8366e4abfa73450d
30isnottoolate <akos.varga.92@gmail.com>
2022-11-28 21:12:03 +0100

    Import Editor component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,13 @@
 import React, { useState } from 'react';
-import DOMPurify from 'dompurify';
 import './App.css';
+import Editor from './Editor';
 
 const App: React.FC = () => {
     const [inputText, setInputText] = useState("");
 
     return (
         <>
-            <div id="text-select-container">
-                <select id="texts" name="texts" size={3}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
-            </div>
-            <div id="text-input-container">
-                <textarea value={inputText} onChange={(event) => setInputText(event.target.value)} />
-            </div>
-            <div id="text-display-container" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(inputText)}} />
+            <Editor />
         </>
     )
 }
