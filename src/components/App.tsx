99cb7032617f3a27dc77c7af6d557ff75c91f6be
30isnottoolate <akos.varga.92@gmail.com>
2022-11-28 19:13:54 +0100

    Add basic app structure

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 const App: React.FC = () => {
     return (
-        <div>
-            So it begins...
-        </div>
+        <>
+            <div id="text-input-container">
+                <textarea />
+            </div>
+            <div id="text-select-container">
+                <select id="texts" name="texts" size={3}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+            </div>
+            <div id="text-display-container" />
+        </>
     )
 }
 
