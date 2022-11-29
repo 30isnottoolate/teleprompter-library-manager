b5484b799e964632ca08e7e32ca9da190f905f1e
30isnottoolate <akos.varga.92@gmail.com>
2022-11-29 16:57:34 +0100

    Add new text functionality

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,6 +5,7 @@ const Editor: React.FC<{ library?: { texts: { text_: { title: string, url: strin
     const [inputText, setInputText] = useState("");
     const [titles, setTitles] = useState([""]);
     const [selectedText, setSelectedText] = useState(1);
+    const [newTextTitle, setNewTextTitle] = useState("");
 
     useEffect(() => {
         if (library) {
@@ -37,7 +38,7 @@ const Editor: React.FC<{ library?: { texts: { text_: { title: string, url: strin
             }
         }));
     }
-    
+
     const saveLibrary = (content: string, fileName: string, contentType: string) => {
         const a = document.createElement("a");
         const file = new Blob([content], { type: contentType });
@@ -45,26 +46,49 @@ const Editor: React.FC<{ library?: { texts: { text_: { title: string, url: strin
         a.download = fileName;
         a.click();
     }
-    
+
     const handleSave = () => {
         saveLibrary(JSON.stringify(library), "librarian.json", "text/plain");
     }
 
+    const handleNewText = () => {
+        let numberOfTitles = library ? Object.keys(library.texts).length : 0;
+        setLibrary(prevState => ({
+            ...prevState,
+            texts: {
+                ...prevState.texts,
+                [`text_${numberOfTitles + 1}`]: {
+                    title: newTextTitle
+                }
+            }
+        }));
+        console.log(library);
+    }
+
     return (
-        <div id="editor">
-            <div id="text-select-container">
-                <select id="texts" name="texts" size={10}>
-                    {titles &&
-                        titles.map((item, index) => <option key={index + 1} value={index + 1} onClick={() => setSelectedText(index + 1)}>{item}</option>)
-                    }
-                </select>
-                <button onClick={handleSave}>Save</button>
+        <>
+            <div id="editor">
+                <div id="text-select-container">
+                    <select id="texts" name="texts" size={10}>
+                        {titles &&
+                            titles.map((item, index) => <option key={index + 1} value={index + 1} onClick={() => setSelectedText(index + 1)}>{item}</option>)
+                        }
+                    </select>
+                    <button onClick={handleSave}>Save</button>
+                    <button>New Text</button>
+                </div>
+                <div id="text-input-container">
+                    <textarea value={inputText} onChange={handleInputChange} />
+                </div>
+                <div id="text-display-container" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(library ? library.texts[`text_${selectedText}`].text : "") }} />
             </div>
-            <div id="text-input-container">
-                <textarea value={inputText} onChange={handleInputChange} />
+            <div>
+                <h3>New Text</h3>
+                <input type="text" value={newTextTitle} onChange={(event) => setNewTextTitle(event.target.value)} />
+                <button onClick={handleNewText}>Save</button>
+                <button>Cancel</button>
             </div>
-            <div id="text-display-container" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(library ? library.texts[`text_${selectedText}`].text : "") }} />
-        </div>
+        </>
     )
 }
 
