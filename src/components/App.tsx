063c2feae52cb16249b316d594247411f3562784
30isnottoolate <akos.varga.92@gmail.com>
2022-11-29 14:18:37 +0100

    Import Startup component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,60 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Startup from './Startup';
 import Editor from './Editor';
 
 const App: React.FC = () => {
     const [mode, setMode] = useState("start"); // start, new, open
-    const [loadedData, setLoadedData] = useState("");
-    const openRef = useRef<HTMLInputElement>(null);
-
-    const triggerClick = () => {
-        if (openRef.current) openRef.current.click();
-    }
-
-    function handleFileOpening(event) {
-        let reader = new FileReader();
-        reader.onload = onFileLoading;
-        reader.readAsText(event.target.files[0]);
-    }
-
-    function onFileLoading(event) {
-        console.log(event.target.result);
-        let jsonData = JSON.parse(event.target.result);
-    }
+    const [library, setLibrary] = useState("");
 
     if (mode === "start") {
-        return (
-            <>
-                <div id="start-menu">
-                    <h1>Library Manager</h1>
-                    <h2>for KV Teleprompter</h2>
-                    <div id="start-buttons">
-                        <button
-                            className="start-button"
-                            onClick={() => { }}
-                        >
-                            New
-                        </button>
-                        <input
-                            id="myFile"
-                            ref={openRef}
-                            onChange={handleFileOpening}
-                            accept=".json"
-                            style={{ display: "none" }}
-                            type="file"
-                            name="filename" />
-                        <button
-                            className="start-button"
-                            onClick={triggerClick}
-                        >
-                            Open
-                        </button>
-                    </div>
-                </div>
-            </>
-        );
+        return <Startup />
     } else if (mode === "new") return <Editor />
-    else return <Editor/>;
+    else return <Editor />;
 
 
 }
