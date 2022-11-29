f9a568a90bcd280602b5cf8d6cc43e6159abc4c0
30isnottoolate <akos.varga.92@gmail.com>
2022-11-29 15:22:57 +0100

    Add basic functionality to Editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,12 @@ import Editor from './Editor';
 
 const App: React.FC = () => {
     const [mode, setMode] = useState("start"); // start, new, open
-    const [library, setLibrary] = useState(null);
+    const [library, setLibrary] = useState({texts: {text_ : {title: "", url: ""}}});
 
     if (mode === "start") {
         return <Startup setLibrary={setLibrary} setMode={setMode} />
     } else if (mode === "new") return <Editor />
-    else return <Editor />;
+    else return <Editor library={library} />;
 
 
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DOMPurify from 'dompurify';
 
-const Editor: React.FC = () => {
+const Editor: React.FC<{ library?: { texts: { text_: { title: string, url: string } } } }> = ({ library }) => {
     const [inputText, setInputText] = useState("");
+    const [titles, setTitles] = useState([""]);
+    const [selectedText, setSelectedText] = useState(1);
+
+    useEffect(() => {
+        if (library) {
+            let titlesHolder = [""];
+
+            for (const item in library.texts) {
+                titlesHolder.push(library.texts[item as keyof typeof library.texts].title);
+            }
+
+            titlesHolder.shift();
+            setTitles(titlesHolder);
+        }
+
+    }, [library]);
+
+    useEffect(() => {
+        console.log(selectedText);
+    }, [selectedText])
 
     return (
         <div id="editor">
             <div id="text-select-container">
-                <select id="texts" name="texts" size={3}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
+                <select id="texts" name="texts" size={10}>
+                    {titles &&
+                        titles.map((item, index) => <option key={index + 1} value={index + 1} onClick={() => setSelectedText(index + 1)}>{item}</option>)
+                    }
                 </select>
             </div>
             <div id="text-input-container">
                 <textarea value={inputText} onChange={(event) => setInputText(event.target.value)} />
             </div>
-            <div id="text-display-container" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(inputText)}} />
+            <div id="text-display-container" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize((library) ? library.texts[`text_${selectedText}`].text : "") }} />
         </div>
     )
 }
