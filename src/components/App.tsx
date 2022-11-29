8055289206bc24b39e8cf235ef3b9df85ed87f5f
30isnottoolate <akos.varga.92@gmail.com>
2022-11-29 13:15:43 +0100

    Add file opening functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import Editor from './Editor';
 
 const App: React.FC = () => {
-    
+    const openRef = useRef<HTMLInputElement>(null);
+
+    const triggerClick = () => {
+        if (openRef.current) openRef.current.click();
+    }
+
+    function handleFileOpening(event) {
+        let reader = new FileReader();
+        reader.onload = onFileLoading;
+        reader.readAsText(event.target.files[0]);
+    }
+
+    function onFileLoading(event) {
+        console.log(event.target.result);
+        let jsonData = JSON.parse(event.target.result);
+    }
+
     return (
         <>
             <div id="start-menu">
                 <h1>Library Manager</h1>
                 <h2>for KV Teleprompter</h2>
                 <div id="start-buttons">
-                    <button className="start-button">New</button>
-                    <button className="start-button">Open</button>
+                    <button className="start-button" onClick={() => {}}>New</button>
+                    <input id="myFile" ref={openRef} onChange={handleFileOpening} accept=".json" style={{ display: "none" }} type="file" name="filename" />
+                    <button className="start-button" onClick={triggerClick}>Open</button>
                 </div>
             </div>
         </>
