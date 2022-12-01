1f2547e49246ba5e16b15556f5323731053e51bb
30isnottoolate <akos.varga.92@gmail.com>
2022-12-01 16:45:05 +0100

    Remove Startup component input

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-import Startup from './Startup';
 import Editor from './Editor';
 
 const App: React.FC = () => {
-    const [mode, setMode] = useState("start"); // start, edit
     const [library, setLibrary] = useState<{ texts: [{ title: string, content: string }] }>({ texts: [{ title: "My First Text", content: "" }] });
 
-    if (mode === "start") {
-        return <Startup setLibrary={setLibrary} setMode={setMode} />
-    } else return <Editor library={library} setLibrary={setLibrary} />
+    return <Editor library={library} setLibrary={setLibrary} />
 }
 
 export default App;
