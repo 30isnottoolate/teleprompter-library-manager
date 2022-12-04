3b25f4533ee9dc3a849aa649305a4bb759e6cc35
30isnottoolate <akos.varga.92@gmail.com>
2022-12-04 22:37:18 +0100

    Add tooltip to Icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,22 +1,33 @@
-import React, { MouseEventHandler } from 'react';
+import React, { useRef, MouseEventHandler } from 'react';
 import icons from '../utilities/icons';
 
 interface IconProps {
-    icon: string, 
-    size: number, 
-    disabled?: boolean, 
-    viewBox?: string, 
+    icon: string,
+    size: number,
+    toolTipText: string,
+    disabled?: boolean,
+    viewBox?: string,
     clickHandler: MouseEventHandler
 }
 
-const Icon: React.FC<IconProps> = ({ icon, size, disabled, viewBox, clickHandler }) => {
+const Icon: React.FC<IconProps> = ({ icon, size, toolTipText, disabled, viewBox, clickHandler }) => {
+    const tooltipRef = useRef<HTMLParagraphElement>(null);
+
     return (
-        <button className="icon"
-            onClick={clickHandler} disabled={disabled} style={{backgroundColor: "#353535", height: size, borderWidth: "0"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" height={size} fill="#bfbfbf" viewBox={viewBox ? viewBox : "0 0 16 16"}>
-                {icons[icon]}
-            </svg>
-        </button>
+        <>
+            <button className="icon"
+                onClick={clickHandler} disabled={disabled} style={{ backgroundColor: "#353535", height: size, borderWidth: "0" }}>
+                <svg xmlns="http://www.w3.org/2000/svg" height={size} fill="#bfbfbf" viewBox={viewBox ? viewBox : "0 0 16 16"}>
+                    {icons[icon]}
+                </svg>
+            </button>
+            <p
+                ref={tooltipRef}
+                className="icon-tooltip"
+            >
+                {toolTipText}
+            </p>
+        </>
     );
 }
 
