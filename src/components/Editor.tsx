3fb29718b82b8e851b9c2ba1562cb990d0619242
30isnottoolate <akos.varga.92@gmail.com>
2022-12-05 18:32:32 +0100

    Add highlightContent function

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import typeSafeProp from '../utilities/typeSafeProp';
 
 interface ExplorerProps {
@@ -9,6 +9,7 @@ interface ExplorerProps {
 }
 
 const Editor: React.FC<ExplorerProps> = ({ library, setLibrary, selectedText, setFileModified }: ExplorerProps) => {
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         let newLibraryTexts = [...library.texts];
@@ -38,10 +39,35 @@ const Editor: React.FC<ExplorerProps> = ({ library, setLibrary, selectedText, se
         setFileModified(true);
     }
 
+    const highlightContent = () => {
+        if (textareaRef.current) {
+            let selectionStart = textareaRef.current.selectionStart;
+            let selectionEnd = textareaRef.current.selectionEnd;
+
+            if (selectionStart !== selectionEnd) {
+                let currentContent = library.texts[selectedText].content;
+                let newLibraryTexts = [...library.texts];
+
+                let topOfContent = currentContent.slice(0, selectionStart);
+                let selectedContent = currentContent.slice(selectionStart, selectionEnd);
+                let bottomOfContent = currentContent.slice(selectionEnd);
+
+                newLibraryTexts[selectedText].content = topOfContent + "<i>" + selectedContent + "</i>" + bottomOfContent;
+
+                setLibrary((prevState: typeof library) => ({
+                    ...prevState,
+                    texts: [
+                        ...newLibraryTexts
+                    ]
+                }));
+            }
+        }
+    }
+
     return (
         <div id="editor">
             <div className="mini-toolbar"></div>
-            <p className="section-label">EDITOR</p>
+            <p className="section-label" onClick={highlightContent}>EDITOR</p>
             <input
                 type="text"
                 value={typeSafeProp(library, selectedText, "title")}
@@ -51,6 +77,7 @@ const Editor: React.FC<ExplorerProps> = ({ library, setLibrary, selectedText, se
             />
             <textarea
                 className="scrollbar"
+                ref={textareaRef}
                 value={typeSafeProp(library, selectedText, "content")}
                 onChange={handleContentChange}
                 placeholder="Type content here..."
