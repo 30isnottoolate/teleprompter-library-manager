4602b30a286afec6a891c162f826e362017c04f4
30isnottoolate <akos.varga.92@gmail.com>
2022-12-05 01:16:40 +0100

    Fix tooltip arrow with SVG image

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -13,6 +13,24 @@ interface IconProps {
 
 const Icon: React.FC<IconProps> = ({ icon, tooltipText, width, height, disabled, viewBox, clickHandler }) => {
     const tooltipRef = useRef<HTMLParagraphElement>(null);
+    const tooltipArrowRef = useRef<SVGSVGElement>(null);
+
+    const handleOnMouseEnter = () => {
+        if (tooltipRef.current) tooltipRef.current.style.opacity = "1";
+        if (tooltipArrowRef.current) tooltipArrowRef.current.style.opacity = "1";
+    }
+
+    const handleOnMouseLeave = () => {
+        if (tooltipRef.current) tooltipRef.current.style.opacity = "0";
+        if (tooltipArrowRef.current) tooltipArrowRef.current.style.opacity = "0";
+    }
+
+    const getArrowTransformValue = () => {
+        if (width) {
+            return `translateX(${(width - 10) / 2}px) translateY(5px)`;
+        } else
+            return `translateX(${(height - 10) / 2}px) translateY(5px)`;
+    }
 
     return (
         <div className="icon-container">
@@ -31,17 +49,27 @@ const Icon: React.FC<IconProps> = ({ icon, tooltipText, width, height, disabled,
                     height={height}
                     fill="#bfbfbf"
                     viewBox={viewBox ? viewBox : "0 0 16 16"}
-                    onMouseEnter={() => { if (tooltipRef.current) tooltipRef.current.style.opacity = "1" }}
-                    onMouseLeave={() => { if (tooltipRef.current) tooltipRef.current.style.opacity = "0" }} >
+                    onMouseEnter={handleOnMouseEnter}
+                    onMouseLeave={handleOnMouseLeave} >
                     {icons[icon]}
                 </svg>
             </button>
             <p
                 ref={tooltipRef}
-                className="icon-tooltip"
-            >
+                className="icon-tooltip">
                 {tooltipText}
             </p>
+            <svg
+                ref={tooltipArrowRef}
+                className="icon-tooltip-arrow"
+                width="10" height="10"
+                style={{
+                    display: "block",
+                    position: "absolute",
+                    transform: getArrowTransformValue()
+                }}>
+                <polygon points="5,0 10,5 0,5" />
+            </svg>
         </div>
     );
 }
